cae5f9292624491631432242145d82f1748d3116
harcomaase <github@marco-haase.de>
2024-02-19 10:28:31 +0100

    convert client to ES module

--- clients/vscode/extension.js
+++ clients/vscode/extension.js
@@ -1,23 +1,21 @@
-const { LanguageClient } = require("vscode-languageclient");
+import { LanguageClient } from "vscode-languageclient";
 
-module.exports = {
-    activate(context) {
-        console.log('hello from lsp-gpt client for vscode!');
-        const command = "lsp-gpt";
+export function activate(context) {
+    console.log('hello from lsp-gpt client for vscode!');
+    const command = "lsp-gpt";
 
-        const serverOptions = {
-            run: { command: command },
-            debug: { command: command },
-        };
-        const clientOptions = {
-            documentSelector: [{ scheme: "file", language: "plantuml" }],
-        };
-        client = new LanguageClient(
-            "lsp-gpt-prototype",
-            "lsp-gpt prototype",
-            serverOptions,
-            clientOptions,
-        );
-        context.subscriptions.push(client.start())
-    },
+    const serverOptions = {
+        run: { command: command },
+        debug: { command: command },
+    };
+    const clientOptions = {
+        documentSelector: [{ scheme: "file", language: "plantuml" }],
+    };
+    client = new LanguageClient(
+        "lsp-gpt-prototype",
+        "lsp-gpt prototype",
+        serverOptions,
+        clientOptions
+    );
+    context.subscriptions.push(client.start());
 }
